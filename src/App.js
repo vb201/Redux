b14976a730289a6280f2b066d670bc96bbb063b9
Vaibhav <berryvaibhav3@gmail.com>
2022-06-12 17:15:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import PostsList from "./components/Post/PostsList";
+import AddPostForm from "./components/Post/Forms/AddPostForm";
+import SinglePostPage from "./components/Post/SinglePostPage";
+import EditPostForm from "./components/Post/Forms/EditPostForm";
+import UsersList from "./components/User/UsersList";
+import UserPage from "./components/User/UserPage";
+import Layout from "./components/Layout";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* Catch Rest */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
